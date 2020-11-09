c4549dfe3a0d34cfdcc8d3af88e7ecea8f610f8e
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-09 17:14:07 -0500

    add before_action to set user for show update and destroy actions

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
   # kept getting "password can't be blank" error without wrap_parameters
   wrap_parameters :user, include: [:email, :username, :password, :password_confirmation]
 
