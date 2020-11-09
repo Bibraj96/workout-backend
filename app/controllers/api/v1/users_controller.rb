c33728008f9450cdfdb5980c544d2561f3fdef9a
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-09 12:55:58 -0500

    add user params set user and a create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,8 +5,22 @@ class Api::V1::UsersController < ApplicationController
     render json: users
   end
 
+  def create
+    byebug
+  end
+
   def show
     
+  end
+
+  private
+
+  def set_user
+    user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :username, :password)
   end
   
 end
