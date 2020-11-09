e5a261201e0fe07adec635eec741980c7adf4de4
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-09 12:57:59 -0500

    add user create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,7 +6,16 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
+    user = User.new(user_params)
     byebug
+    if user.save
+      render json: user, staus: :created
+    else
+      resp = {
+        error: user.errors.full_messages.to_sentence
+      }
+      render json: resp, status :unprocessable_entity
+    end
   end
 
   def show
