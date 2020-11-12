0b6aa7b36376b622c194d2e6b87ab1e3e4de21b1
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-12 15:14:37 -0500

    add workout params and  set workout before show update and destroy actions

--- app/controllers/api/v1/workouts_controller.rb
+++ app/controllers/api/v1/workouts_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::WorkoutsController < ApplicationController
+  before_action :set_workout, only: [:show, :update, :destroy]
 
   def index
     if logged_in?
@@ -9,6 +10,23 @@ class Api::V1::WorkoutsController < ApplicationController
         error: "You must be logged in to view workouts!"
       }
     end
+  end
+
+  def show
+    render json: workout
+  end
+
+  def create
+  end
+
+  private
+
+  def set_workout
+    workout = Workout.find(params[:id])
+  end
+
+  def workout_params
+    params.require(:workout).permit(:title, :date)
   end
   
 end
